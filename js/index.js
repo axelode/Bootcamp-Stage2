07a0bd155ec69ac5fa73b18d47b05296ec6a009e
axelode <endangtryna56@gmail.com>
2024-03-25 22:04:25 +0700

    tugas day 1 typescript

---
+++ js/index.js
@@ -0,0 +1,33 @@
+"use strict";
+function handleStatus(mahasiswa) {
+    if (mahasiswa.nilai === 100 && mahasiswa.presensi >= 80) {
+        return `${mahasiswa.nama} Lulus Cumlaude`;
+    }
+    else if (mahasiswa.nilai >= 70 && mahasiswa.presensi >= 70) {
+        return `${mahasiswa.nama} Lulus`;
+    }
+    else {
+        return `${mahasiswa.nama} Tidak Lulus`;
+    }
+}
+const mahasiswa = [
+    {
+        nama: "Jhon",
+        nilai: 60,
+        presensi: 50
+    },
+    {
+        nama: "Marco",
+        nilai: 100,
+        presensi: 100
+    },
+    {
+        nama: "Bella",
+        nilai: 80,
+        presensi: 90
+    }
+];
+for (let i = 0; i < mahasiswa.length; i++) {
+    const result = handleStatus(mahasiswa[i]);
+    console.log(result);
+}
